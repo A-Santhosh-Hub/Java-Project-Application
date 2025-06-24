5d6d9e20c6713c6439c194db9702be114fb00051
SANTHOSH A <arussanthosh@gmail.com>
2025-06-24 21:26:20 +0530

    Create ResumeData.java
    Add ResumeData.java

---
+++ Online Resume Builder in Java/ResumeData.java
@@ -0,0 +1,14 @@
+public class ResumeData {
+    String name, email, phone, summary, education, experience, skills;
+
+    public ResumeData(String name, String email, String phone,
+                      String summary, String education, String experience, String skills) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.summary = summary;
+        this.education = education;
+        this.experience = experience;
+        this.skills = skills;
+    }
+}
